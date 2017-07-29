95b645b355cdbe7542526ff19401020ca6a45f14
David Barnett <david@barnett.net.nz>
2017-07-29 16:30:43 +1200

    Add traffic incidents at a location

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function weatherCallBack(data) {
   console.log(data);
 }
 
+function getTrafficIncidents(lat, long) {
+  var prox = 7152;
+
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "https://traffic.cit.api.here.com/traffic/6.0/incidents.json?prox=" +
+    lat + "," + long + "," + prox +
+    "&app_id=" + HERE_APP_ID + "&app_code=" + HERE_APP_CODE
+   + "&jsoncallback=trafficCallBack";
+
+  var body = document.getElementById('weather_api');
+  body.appendChild(script);
+}
+
+function trafficCallBack(data) {
+  console.log(data);
+}
+
+function LatLongToMat(lat, long, zoom) {
+  var latRad = lat * Math.PI / 180;
+  var n = Math.pow(2, z);
+  var xTile = n * ((lon + 180) / 360);
+  var yTile = n * (1-(Math.log(Math.tan(latRad) + 1/Math.cos(latRad)) /Math.PI)) / 2;
+
+  return {column: xTile, row: yTile, zoom: zoom};
+}
